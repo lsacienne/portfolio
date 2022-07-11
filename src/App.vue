55e10d80f9ad51787d7186c931a1feff59685877
lsacienne <alexandre.viala@outlook.com>
2022-07-11 17:41:44 +0200

    :sparkles: Add of elements in template markup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <h1>Bienvenue sur le portfolio de Alexandre Viala</h1>
+    <WebsiteNavigation/>
+  </main>
 </template>
 
 <script>
