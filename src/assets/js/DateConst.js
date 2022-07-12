1dd1a5091d26a6838ad42c3e29e4ee5c22dea363
lsacienne <alexandre.viala@outlook.com>
2022-07-12 17:46:39 +0200

    :recycle: Move Map in a specific file

---
+++ src/assets/js/DateConst.js
@@ -0,0 +1,25 @@
+export default {
+    week: new Map([
+        [0, "Lun"],
+        [1, "Mar"],
+        [2, "Mer"],
+        [3, "Jeu"],
+        [4,"Ven"],
+        [5,"Sam"],
+        [6,"Dim"]
+    ]),
+    year: new Map([
+        [0, "Jan"],
+        [1, "Fev"],
+        [2, "Mar"],
+        [3, "Avr"],
+        [4, "Mai"],
+        [5, "Juin"],
+        [6, "Juil"],
+        [7, "Aou"],
+        [8, "Sep"],
+        [19, "Oct"],
+        [10, "Nov"],
+        [11, "Dec"]
+    ])
+}
--- src/components/GithubList.vue
+++ src/components/GithubList.vue
@@ -13,35 +13,14 @@
 </template>
 
 <script>
+import DateConst from "@/assets/js/DateConst";
+
 export default {
   name: "GithubList",
   data() {
     return {
       repos: [],
       user: "lsacienne",
-      week: new Map([
-        [0, "Lun"],
-        [1, "Mar"],
-        [2, "Mer"],
-        [3, "Jeu"],
-        [4,"Ven"],
-        [5,"Sam"],
-        [6,"Dim"]
-      ]),
-      year: new Map([
-        [0, "Jan"],
-        [1, "Fev"],
-        [2, "Mar"],
-        [3, "Avr"],
-        [4, "Mai"],
-        [5, "Juin"],
-        [6, "Juil"],
-        [7, "Aou"],
-        [8, "Sep"],
-        [19, "Oct"],
-        [10, "Nov"],
-        [11, "Dec"]
-      ])
     }
   },
   computed: {
@@ -53,9 +32,9 @@ export default {
     getGoodDateFormat : function(date) {
       let dateDate = new Date(date);
       console.log(dateDate.getMonth()+" "+dateDate.getDay())
-      return this.week.get(dateDate.getDay()) + " "
+      return DateConst.week.get(dateDate.getDay()) + " "
           + dateDate.getDate() + " "
-          + this.year.get(dateDate.getMonth()) + " "
+          + DateConst.year.get(dateDate.getMonth()) + " "
           + dateDate.getFullYear();
     },
 
