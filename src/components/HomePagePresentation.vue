40c69c5e3ddbdb7722b86425061510c45f7f1e04
lsacienne <alexandre.viala@outlook.com>
2022-07-12 17:56:20 +0200

    :sparkles: Add of new component with a future content. I will probably put my useful network here.

---
+++ src/components/HomePagePresentation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="presentation">
+    <h1>Présentation</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePagePresentation"
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 2rem;
+    font-family: Helvetica,cursive;
+    color: whitesmoke;
+  }
+
+  .presentation {
+    font-size: 1rem;
+    text-align: justify;
+  }
+</style>
