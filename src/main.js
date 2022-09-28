f00bb700dcfa5332bf2179bb9e0ca063d58fd19b
lsacienne <alexandre.viala@outlook.com>
2022-09-28 21:23:35 +0200

    add of main js file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createVuesticEssential, VaButton } from 'vuestic-ui'
+import 'vuestic-ui/styles/essential.css'
+import 'vuestic-ui/styles/reset.css'
 
-createApp(App).mount('#app')
+createApp(App).use(createVuesticEssential({ components: { VaButton } })).mount('#app')
