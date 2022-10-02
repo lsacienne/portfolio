1f15126e42317b1648d18e170faeab2a82e57bda
lsacienne <alexandre.viala@outlook.com>
2022-10-03 00:29:07 +0200

    :heavy_plus_sign: Add of vue-router

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import { createVuesticEssential, VaButton } from 'vuestic-ui'
 import 'vuestic-ui/styles/essential.css'
 import 'vuestic-ui/styles/reset.css'
+import router from './router'
 
-createApp(App).use(createVuesticEssential({ components: { VaButton } })).mount('#app')
+createApp(App).use(router).use(createVuesticEssential({ components: { VaButton } })).mount('#app')
