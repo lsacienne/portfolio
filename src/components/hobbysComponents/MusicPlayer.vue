2d41d30481404e897877bc928cad99e326a01e9c
lsacienne <alexandre.viala@outlook.com>
2022-10-09 21:17:58 +0200

    :wrench: Fix CORS issue on production

--- src/components/hobbysComponents/MusicPlayer.vue
+++ src/components/hobbysComponents/MusicPlayer.vue
@@ -69,7 +69,7 @@ import SemipolarSpinner from '../SemipolarSpinner.vue';
             
             
             let _url = process.env.NODE_ENV === "production" ? 
-            `https://api.deezer.com/search/?q=${song_title}&index=0&limit=2&output=json`
+            `http://cors.io/?https://api.deezer.com/search/?q=${song_title}&index=0&limit=2&output=json`
              : `/deezerapi/search/?q=${song_title}&index=0&limit=2&output=json`;
             
             let response = await fetch(encodeURI(_url));
