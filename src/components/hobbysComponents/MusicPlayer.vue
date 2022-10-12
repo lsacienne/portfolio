213b3186673f3dd87b99a313746b7e3102d8f22e
lsacienne <alexandre.viala@outlook.com>
2022-10-12 13:29:29 +0200

    :iphone: Handle two types of events without tiggering the other

--- src/components/hobbysComponents/MusicPlayer.vue
+++ src/components/hobbysComponents/MusicPlayer.vue
@@ -21,7 +21,7 @@
                 </button>
             </li>
             <li>
-                <button aria-label="play-pause" class="button-play button" v-on:touchend="playMusic" v-on:click="playMusic">
+                <button aria-label="play-pause" class="button-play button" v-on:touchstart="playMusic" v-on:mousedown="playMusic">
                     <img src="@/assets/icons/play_icon.svg" alt="play and pause button">
                 </button>
             </li>
