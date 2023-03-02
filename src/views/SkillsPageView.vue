2ee3f7690002ddf0b02cf5572777d975d1adca2f
lsacienne <alexandre.viala@outlook.com>
2023-03-02 23:30:22 +0100

    :sparkles: Add of languages on skills page

--- src/views/SkillsPageView.vue
+++ src/views/SkillsPageView.vue
@@ -1,14 +1,112 @@
 <template>
     <div class="container">
-        
+        <LanguageBadge languageName="Vue-3" imgSource="vue-logo.png" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="CSS" imgSource="logo-css.png" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="HTML" imgSource="logo-html.png" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="Javascript" imgSource="Unofficial_JavaScript_logo_2 1.png" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="Typescript" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="C" imgSource="c_programming_language.png" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="C++" imgSource="Cpp_Logo.png" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="C#" ImgSource="c-sharp-programming-language-icon.png" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="Unity" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="Python" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="Java" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="Kotlin" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="SQL, MySQL, SQLite" imgSource="SQLite370.png" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="PHP" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="Android" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
+        <LanguageBadge languageName="Figma" :badgesList="['https://github.com/lsacienne/calendar-maker','https://github.com/lsacienne/portfolio']">
+            Vue 3 est un framework front-end pour le langage Javascript. Il permet de concevoir
+            des sites internets front-end.<br>
+            J'ai eu l'ocassion d'utiliser ce framework afin de réaliser ce portfolio ainsi que
+            la deuxième version de mon logiciel calendar-maker.
+        </LanguageBadge>
     </div>
 </template>
 <script>
+import LanguageBadge from '@/components/skills/LanguageBadge.vue';
+
 export default {
     name: "SkillsPageView",
     data() {
         return {};
     },
+    components: { LanguageBadge }
 }
 </script>
 
